78d05c03fd9b02ee5ed630b3a6e2822cf7f74053
Sona Subramanian <sonas0682@gmail.com>
2023-02-28 16:26:55 -0600

    Initial commit

---
+++ src/main/java/com/sa/sa1/Sa1Application.java
@@ -0,0 +1,13 @@
+package com.sa.sa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sa/sa1/Sa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.sa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
